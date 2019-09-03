36c9a0c354c8fb9ce6d6679399e313bdfcaf927d
Playerian <you@example.com>
2019-09-02 21:14:19 -0700

    make circle round again

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //vars
 var clickable = false;
+var unitClickable = false;
 
 //opening the door
 $("#gear").click(function(){
@@ -48,7 +49,7 @@ $("#gear").click(function(){
                         $("#gate").css('z-index', '-10');
                         $('.schwartz').show();
                         //right mover
-                        $('#gear').animate({"margin-right": '-=70vw'}, 5000);
+                        $('#gear').animate({"margin-left": '+=70vw'}, 5000);
                         $('#horizontalLightRight').animate({"left": '+=70%'}, 5000);
                         $('#verticleLightTop').animate({"left": '+=70%'}, 5000);
                         $('#verticleLightBot').animate({"left": '+=70%'}, 5000);
@@ -82,6 +83,9 @@ function mainOpening(){
         $("#botContent").animate({"left": "0%"}, 4000, function(){
             clickable = true;
             $('body').css("background", "#414947");
+            //remove gears and set width
+            $(".gear").remove();
+            $('.parent').css('width', '100%');
         });
     }, 3000);
     //top show
@@ -96,6 +100,7 @@ $("#topClicker").click(function(){
         $("#midContent, #botContent").hide();
         $("#topContent").css('height', "100vh");
         $("#topContentContainer").show();
+        unitClickable = true;
     }
 });
 
@@ -110,6 +115,12 @@ $(".returnBack").click(function(){
     }
 });
 
+$("#unit2Clicker").click(function(){
+
+});
+
+$('#topContentContainer, #midContentContainer, #botContentContainer').hide();
+
 //test purpose
 // mainOpening();
-// window.scrollTo(0, $(document).height() - window.innerHeight);
+//window.scrollTo(0, $(document).height() - window.innerHeight);
